d9a70ceed75ea3477573e62fc324a1685f81bb3e
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-05 15:04:43 -0600

    Update PostListComponent code.

---
+++ src/app/components/post-list/post-list.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit{
+
+  // Variable to set the PostListComponent title
+  postListTitle: string = 'Post List Component';
+
+  objectArray: Array<any> = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  addPost() {
+    this.objectArray.push({id: 5, title: 'New post', content: 'My new post added'});
+  }
+
+  deletePost(index: number) {
+    this.objectArray.splice(index, 1);
+  }
+}
