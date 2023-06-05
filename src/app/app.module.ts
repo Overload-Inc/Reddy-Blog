daeef7614ba5c410d871482f2e806ce04d6e8e4a
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-05 15:02:07 -0600

    Update UserComponent declaration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostComponent } from './components/post/post.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import {FormsModule} from "@angular/forms";
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [ // Specifies all the components that will be used in the application
     AppComponent,
     NavbarComponent,
     PostComponent,
-    PostListComponent
+    PostListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
