34fac18fe89b46be00e58b23a424f3cdbd25bd28
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-05 15:03:13 -0600

    Delete code related to Post and PostList components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css'] // Establishes the styles used for the component
 })
 export class AppComponent {
-  postTitle: string = "";
-  postDetails: string = "";
-  imageURL: string = "";
-  postURL: string = "";
-  addBackground: boolean = false;
 
-  postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
-
-  objectArray: Array<any> = [];
-
-  stepForm: string = "Click a button";
-
-  isActive: boolean = true;
-
-  constructor() {
-  }
-
-  addPost() {
-    this.objectArray.push({id: 5, title: 'New post', content: 'My new post added'});
-  }
-
-  deletePost(index: number) {
-    this.objectArray.splice(index, 1);
-  }
-
-  showStep(step: string) {
-    this.stepForm = step;
-  }
 }
