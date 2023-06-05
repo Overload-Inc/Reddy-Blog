425f62f1c20efe72aa3c71924856fdc59c1bdf8c
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-04 22:07:35 -0600

    Update application title variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Establishes the name of the component to be used outside
+  templateUrl: './app.component.html', // Establishes the place which provides the look & feel for the component
+  styleUrls: ['./app.component.css'] // Establishes the styles used for the component
 })
 export class AppComponent {
-  title = 'reddy-blog-site';
+  // Variable to set the title of the AppComponent
+  applicationTitle: string = 'Reddy Blog Site';
+
+  constructor() {
+  }
 }
