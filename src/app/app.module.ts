290549e728197276bf014e81072d075bde9aaa4c
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-04 22:06:49 -0600

    Update component declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostComponent } from './components/post/post.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // Specifies all the components that will be used in the application
+    AppComponent,
+    NavbarComponent,
+    PostComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
