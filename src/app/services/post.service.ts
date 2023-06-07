252fdedd9cde79a7592151dfcb64882a7e2a8e07
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-07 00:34:31 -0600

    Delete post list items

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Post} from "../models/post";
+import { Post } from "../models/post";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  postList: Array<Post> = [
-    {id: 1, title:"Post 1", content:"My first post"},
-    {id: 2, title:"Post 2", content:"My second post"},
-    {id: 3, title:"Post 3", content:"My third post"},
-    {id: 4, title:"Post 4", content:"My fourth post"},
-  ]
+  postList: Array<Post> = [];
 
   constructor() { }
 
-  addPost(post: any) {
+  addPost(post: Post) {
     this.postList.push(post);
   }
 
