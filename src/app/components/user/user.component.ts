36f3f0e213052225c1aeca88af6cd4af41291283
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-06 21:54:04 -0600

    Update ReactiveForms, FormGroups and Nested FormGroups

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/user";
-import {FormControl, NgForm} from "@angular/forms";
+import {FormArray, FormControl, NgForm, PatternValidator, Validators} from "@angular/forms";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user',
@@ -12,39 +13,71 @@ export class UserComponent implements OnInit {
 
   componentTitle: string = "Users Form";
 
-  fullName: string = "";
-  email: string = "";
-  address: string = "";
-
   userList: Array<User>;
+  userForm: any;
+  emailRegex: string = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$";
+  contactRegex: string = "[789][0-9]{9}";
 
   constructor(private user: UserService) {
     this.userList = user.userList;
+
+    this.userForm = new FormGroup({
+      fullName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      contactDetails: new FormGroup({
+        address: new FormControl('', [Validators.required]),
+        shippingAddress: new FormControl('', [Validators.required]),
+        contactNumber: new FormControl('', [Validators.required, Validators.pattern(this.contactRegex)])
+      }),
+      skills: new FormArray([])
+    });
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(userForm: NgForm) {
-    this.fullName = userForm.value.fullName;
-    this.email = userForm.value.email;
-    this.address = userForm.value.address;
-
-    this.addUser();
+  get fullName() {
+    return this.userForm.get('fullName');
   }
 
-  getValue(fullName: any) {
-    console.log(fullName);
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get address() {
+    return this.userForm.get('contactDetails.address');
+  }
+
+  get shippingAddress() {
+    return this.userForm.get('contactDetails.shippingAddress');
+  }
+
+  get contactNumber() {
+    return this.userForm.get('contactDetails.contactNumber');
+  }
+
+  get skills() {
+    return this.userForm.get('skills') as FormArray;
   }
 
   addUser() {
     const user = {
-      fullName: this.fullName, email: this.email, address: this.address};
+      fullName: this.fullName.value, email: this.email.value, address: this.address.value};
 
     this.user.addUser(user);
   }
 
+  addSkills(skill: HTMLInputElement) {
+    this.skills.push(new FormControl(skill.value));
+
+    skill.value = '';
+  }
+
   deleteUser(i: number) {
     this.user.deleteUser(i);
+  }
+
+  removeSkill(index: number) {
+    this.skills.removeAt(index);
   }
 }
